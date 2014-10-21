0cddb022e3f54314c26f2a812b7153b54ee7a8e2
jimmy-sv <s.vertepniy@gmail.com>
2014-10-21 15:34:12 +0300

    add model and service data

--- src/main/java/com/gmplaces/models/Address.java
+++ src/main/java/com/gmplaces/models/Address.java
@@ -2,4 +2,32 @@ package com.gmplaces.models;
 
 
 public class Address {
+
+    private long lat;
+    private long lng;
+    private String description;
+
+    public  Address (long lat, long lng, String desc){
+        this.lat = lat;
+        this.lng = lng;
+        this.description = desc;
+    }
+
+    public  Address (long lat, long lng){
+        this.lat = lat;
+        this.lng = lng;
+        this.description =  "";
+    }
+
+    public long getLat() {
+        return lat;
+    }
+
+    public long getLng() {
+        return lng;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- src/main/java/com/gmplaces/models/IDataService.java
+++ src/main/java/com/gmplaces/models/IDataService.java
@@ -1,12 +1,13 @@
 package com.gmplaces.models;
 
+import java.util.List;
 
 public interface IDataService {
 
-    public String putData(long lng,long ltd, String description);
+    public String putData(long lat,long lng, String description);
+
+    public List<Address> getData();
 
     public String clearData();
-
-
 
 }
