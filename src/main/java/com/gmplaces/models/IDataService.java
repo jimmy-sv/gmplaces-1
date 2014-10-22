c42dfc72b1215be24673660d498b615518879c0c
roma_wint <roma_wint@mail.ru>
2014-10-22 16:55:48 +0300

    Add model

--- src/main/java/com/gmplaces/models/IDataService.java
+++ src/main/java/com/gmplaces/models/IDataService.java
@@ -2,12 +2,21 @@ package com.gmplaces.models;
 
 import java.util.List;
 
+/**
+ * Created by roman vintonyak on 22.10.14.
+ */
 public interface IDataService {
 
+    //put new address into file
+    //returns status of operation
     public String putData(Address address);
 
+    //return all data
     public List<Address> getData();
 
-    public String clearData();
+    //clear all data
+    public void clearData();
 
+    //remove specific address from file
+    public String removeData(Address address);
 }
